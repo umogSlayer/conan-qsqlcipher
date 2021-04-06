f6c32021c39640aa1ef07c1d228e82f1afd4e71e
Vladimir Bayrashevskiy <umogSlayer@users.noreply.github.com>
2021-04-05 23:59:42 -0700

    Implement Win32 build + package creation

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile
+from conans import ConanFile, MSBuild
 from conans import tools
 import os, shutil
 
@@ -9,7 +9,7 @@ class QSqlCipherConan(ConanFile):
     settings = 'os', 'compiler', 'build_type', 'arch'
     generators = 'qmake'
     requires = ['sqlcipher/4.4.2', 'qt/5.15.2@bincrafters/stable']
-    exports = ["conan-qsqlcipher.pro", "patches/*.patch"]
+    exports = ["patches/*.patch"]
 
     def source(self):
         sources_git = tools.Git(folder='qsqlcipher')
@@ -22,9 +22,20 @@ class QSqlCipherConan(ConanFile):
 
     def build(self):
         tools.patch(base_path="qsqlcipher", patch_file="patches/qsqlcipher.pro-%s.patch" % self.version, strip=1)
-        with tools.vcvars(self.settings) if self.settings.compiler == "Visual Studio" else tools.no_op():
-            self.run("qmake CONFIG+=\"staticlib conan-sqlcipher\" qsqlcipher/qsqlcipher.pro", run_environment=True)
-        if self.settings.compiler != "Visual Studio":
-            self.run(self._make_program(), run_environment=True)
+        if self.settings.compiler == "Visual Studio":
+            with tools.vcvars(self.settings):
+                self.run("qmake -spec win32-msvc -tp vc CONFIG+=\"staticlib conan-sqlcipher\" qsqlcipher\\qsqlcipher\\qsqlcipher.pro", run_environment=True)
+            msbuild = MSBuild(self)
+            msbuild.build("qsqlcipher.vcxproj")
         else:
-            pass
+            self.run("qmake CONFIG+=\"staticlib conan-sqlcipher\" qsqlcipher/qsqlcipher.pro", run_environment=True)
+            self.run(self._make_program(), run_environment=True)
+
+    def package(self):
+        if self.settings.compiler == "Visual Studio":
+            self.copy('*.lib', dst='lib', src='plugins/sqldrivers')
+        else:
+            self.copy('*.a', dst='lib', src='qsqlcipher/plugins/sqldrivers')
+
+    def package_info(self):
+        self.cpp_info.libs = ['qsqlcipher']
