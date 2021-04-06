be7c31c3472e6f03f3bcfb3d01dc6aa2cdfd3144
Vladimir Bayrashevskiy <umogSlayer@users.noreply.github.com>
2021-04-06 12:39:26 +0700

    Implement Linux build

--- conanfile.py
+++ conanfile.py
@@ -4,15 +4,27 @@ import os, shutil
 
 class QSqlCipherConan(ConanFile):
     name = 'qsqlcipher'
-    branch = '5.15'
+    version = '5.15-3'
     url = 'https://github.com/umogSlayer/conan-qsqlcipher'
-    version = '1.0' + branch
     settings = 'os', 'compiler', 'build_type', 'arch'
     generators = 'qmake'
-    requires = ['sqlcipher/4.4.2']
+    requires = ['sqlcipher/4.4.2', 'qt/5.15.2@bincrafters/stable']
+    exports = ["conan-qsqlcipher.pro", "patches/*.patch"]
 
     def source(self):
         sources_git = tools.Git(folder='qsqlcipher')
         sources_git.clone('https://github.com/sjemens/qsqlcipher-qt5.git',
-                          branch=self.branch,
+                          branch='v%s' % self.version,
                           shallow=True)
+
+    def _make_program(self):
+        return "make"
+
+    def build(self):
+        tools.patch(base_path="qsqlcipher", patch_file="patches/qsqlcipher.pro-%s.patch" % self.version, strip=1)
+        with tools.vcvars(self.settings) if self.settings.compiler == "Visual Studio" else tools.no_op():
+            self.run("qmake CONFIG+=\"staticlib conan-sqlcipher\" qsqlcipher/qsqlcipher.pro", run_environment=True)
+        if self.settings.compiler != "Visual Studio":
+            self.run(self._make_program(), run_environment=True)
+        else:
+            pass
